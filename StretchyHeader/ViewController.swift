1b23178a66175680f5e8311260e291b4efdd68b9
ikechukwuhenry <ikechukwuhenry@ymail.com>
2018-12-30 11:59:40 +0100

    Initial Commit

---
+++ StretchyHeader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StretchyHeader
+//
+//  Created by ikechukwu Michael on 30/12/2018.
+//  Copyright © 2018 ikechukwu Michael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ StretchyHeaderTests/StretchyHeaderTests.swift
@@ -0,0 +1,34 @@
+//
+//  StretchyHeaderTests.swift
+//  StretchyHeaderTests
+//
+//  Created by ikechukwu Michael on 30/12/2018.
+//  Copyright © 2018 ikechukwu Michael. All rights reserved.
+//
+
+import XCTest
+@testable import StretchyHeader
+
+class StretchyHeaderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
